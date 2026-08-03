91ee5ad97ed7b2800c028cbeb546d25099f9d29d
sor999 <sorsor999@naver.com>
2026-08-03 16:58:26 +0900

    chore: github pages에 맞게 base 경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: '/pomodoro/',
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
